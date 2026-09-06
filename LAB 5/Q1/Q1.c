b5fba04b633f6b9e00cc5a7cbac12f29848c7579
bbek2006 <bibekkodamasingh676@gmail.com>
2026-09-06 14:14:39 +0530

    Add quickSelect function to find median of array
    Implement quickSelect algorithm to find the median.

---
+++ LAB 5/Q1/Q1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[high]);
+    return i;
+}
+
+int quickSelect(int arr[], int low, int high, int k) {
+    if (low <= high) {
+        int pi = partition(arr, low, high);
+
+        if (pi == k)
+            return arr[pi];
+        else if (pi > k)
+            return quickSelect(arr, low, pi - 1, k);
+        else
+            return quickSelect(arr, pi + 1, high, k);
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter elements:\n");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int median = quickSelect(arr, 0, n - 1, n / 2);
+    printf("Median: %d\n", median);
+
+    return 0;
+}
